712ac160c46aee977591fe7aab9914f6b3057957
Bjolek <141151371+Bjolek@users.noreply.github.com>
2024-05-01 18:39:48 +0300

    ЕКРАН 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 
@@ -15,9 +16,12 @@ class FirstScr(Screen):
         btn1.bind(on_press=self.goToUkrScreen1)
         btn2 = Button(text="екран 2")
         btn2.bind(on_press=self.goToUkrScreen2)
+        btn3 = Button(text="екран 3")
+        btn3.bind(on_press=self.goToUkrScreen3)
 
         vbox.add_widget(btn1)
         vbox.add_widget(btn2)
+        vbox.add_widget(btn3)
 
         hbox.add_widget(txt)
         hbox.add_widget(vbox)
@@ -29,6 +33,9 @@ class FirstScr(Screen):
 
     def goToUkrScreen2(self, instance):
         self.manager.current = "екран 2"
+
+    def goToUkrScreen3(self, instance):
+        self.manager.current = "екран 3"
 
 
 class UkrScreen1(Screen):
@@ -59,19 +66,38 @@ class UkrScreen2(Screen):
         self.manager.current = "first"
 
 
+class UkrScreen3(Screen):
+    def __init__(self, name="екран 3"):
+        super().__init__(name=name)
+        hbox = BoxLayout(orientation="vertical")
+
+        btn_back = Button(text="Назад")
+        btn_back.bind(on_press=self.goBackToFirst)
+
+        txt_input = TextInput(hint_text="Введіть текст")
+
+        hbox.add_widget(btn_back)
+        hbox.add_widget(txt_input)
+
+        self.add_widget(hbox)
+
+    def goBackToFirst(self, instance):
+        self.manager.current = "first"
+
+
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
 
-
         first_screen = FirstScr()
         ukr_screen_1 = UkrScreen1()
         ukr_screen_2 = UkrScreen2()
-
+        ukr_screen_3 = UkrScreen3()
 
         sm.add_widget(first_screen)
         sm.add_widget(ukr_screen_1)
         sm.add_widget(ukr_screen_2)
+        sm.add_widget(ukr_screen_3)
 
         return sm
 
